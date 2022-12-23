f5f0b86a0eef9cba430b83d6b10d09094d1a4b60
Sujay V <sujaywebdev@gmail.com>
2022-12-23 15:16:27 +0530

    Creating BigPromise

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,6 +1,18 @@
-exports.home = (req,res) => {
+const BigPromise = require('../middlewares/bigPromise')
+
+exports.home = BigPromise(async(req,res) => {
+    // const dbConnec = await apiCall()
     res.status(200).json({
         success:true,
         greeting:"Hello this is a Sample Backend"
     })
-}
+})
+
+
+
+
+
+
+
+
+
---
+++ middlewares/bigPromise.js
@@ -0,0 +1,4 @@
+// Try Catch and async - await || use Promise
+
+module.exports = func => (req,res,next) => 
+    Promise.resolve(func(req,res,next)).catch(next)
